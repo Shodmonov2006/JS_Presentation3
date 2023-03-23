296286a587e4602d312effc1991cbabd32a1e6f0
Shodmonov2006 <olimkhujashodmonovv@gmail.com>
2023-03-23 11:05:47 +0300

    presentation3

---
+++ JavaScript.js
@@ -0,0 +1,25 @@
+//////////METOD repeat()
+//1
+function tets1(){
+    let x='hello Guys'
+    let x1=x.repeat(3)
+    return x1
+}
+console.log(tets1())
+
+
+//2
+function test2(){
+    let x='hello World'
+    let x1=x.repeat(5)
+    return x1
+}
+console.log(test2())
+
+//3
+function test3(){
+    let x='soft club'
+    let x1=x.repeat('10')
+    return x1
+}
+console.log(test3())
